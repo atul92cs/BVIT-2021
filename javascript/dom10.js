7137f16d9262a6615e838bb1c6d1ec93f6467fda
Atul kumar singh <atul.11192@gmail.com>
2021-08-12 11:44:18 +0530

    adding assignment 5

--- javascript/dom10.js
+++ javascript/dom10.js
@@ -4,11 +4,11 @@ addmovie=(e)=>{
    //let director=document.querySelector('#director').value;
    // let genre=document.querySelector('#genre').value;
     let name=document.getElementById('name').value;
-    let url='http://localhost:8080/director/add';
+    let url='http://localhost/director/add';
     let xhr=new XMLHttpRequest();
     xhr.open('POST',url,true);
     let params=`name= ${name}`;
-    xhr.setRequestHeader('Content-type','application/x-form-urlencoded');
+   // xhr.setRequestHeader('Content-type','application/x-form-urlencoded');
     xhr.onload=()=>{
         if(xhr.status==200||xhr.readyState==4)
         {
