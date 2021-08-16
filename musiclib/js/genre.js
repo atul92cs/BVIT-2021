4c899ae672070854b3890e2ac02ff36fa254db85
Atul kumar singh <atul.11192@gmail.com>
2021-08-16 15:49:03 +0530

    adding more code

---
+++ musiclib/js/genre.js
@@ -0,0 +1,65 @@
+getgenre=()=>{
+    let url='http://localhost/musiclib/getgenre.php';
+    let xhr=new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.onload=()=>{
+        if(xhr.status==200||xhr.readyState==4)
+        {
+            let response=JSON.parse(xhr.responseText);
+            let {genre}=response;
+            genre.forEach(gr=>{
+                let genrelist=document.querySelector('#genre');
+                let geel=document.createElement('option');
+                let eltxt=document.createTextNode(gr.name);
+                 geel.value=gr.id;
+                 geel.appendChild(eltxt);
+                 genrelist.appendChild(geel);
+            
+            });
+        }
+        else
+        {
+            console.log('error occured');
+        }
+    }
+    xhr.send();
+}
+getgenre();
+createArtist=(e)=>{
+    let url='http://localhost/musiclib/createartist.php';
+    e.preventDefault();
+    let name=document.querySelector('#name').value;
+    let genre=document.querySelector('#genre').value;
+    let data={
+        "name":name,
+        "genre":genre
+    };
+    let params=JSON.stringify(data);
+    let xhr=new XMLHttpRequest();
+    xhr.open('POST',url,true);
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.onload=()=>{
+        if(xhr.readyState==4||xhr.responseText==200)
+        {
+            let message=document.createElement('h2');
+            let msgtxt=document.createTextNode('Artist created');
+            let msgarea=document.querySelector('#message-area');
+            message.appendChild(msgtxt);
+            msgarea.appendChild(message);
+        }
+        else
+        {
+            let message=document.createElement('h2');
+            let msgtxt=document.createTextNode('Error occured');
+            let msgarea=document.querySelector('#message-area');
+            message.appendChild(msgtxt);
+            msgarea.appendChild(message);
+        
+
+        }
+    }
+    xhr.send(params);
+}
+let form=document.querySelector('#artist-form');
+form.addEventListener('submit',createArtist);
