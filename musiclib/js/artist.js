c4d1a4dcec65b31616673bac91f21ed42ce8f74d
Atul kumar singh <atul.11192@gmail.com>
2021-08-17 12:18:38 +0530

    adding more code

--- musiclib/js/artist.js
+++ musiclib/js/artist.js
@@ -14,10 +14,36 @@ getArtist=()=>{
             let artiname=document.createTextNode(ar.name);
             let genlabel=document.createElement('p');
             let genname=document.createTextNode(ar.genre);
+            let deletebtn=document.createElement('button');
+            let deletebtntxt=document.createTextNode('delete');
+            deletebtn.appendChild(deletebtntxt);
             artilabel.appendChild(artiname);
             genlabel.appendChild(genname);
             artistbody.appendChild(artilabel);
             artistbody.appendChild(genlabel);
+            artistbody.appendChild(deletebtn);
+            let url2='http://localhost/musiclib/deleteartist.php';
+            deletebtn.addEventListener('click',(e)=>{
+                e.preventDefault();
+                let id=ar.id;
+                let data={
+                    "id":id
+                };
+                let params=JSON.stringify(data);
+                let xhr=new XMLHttpRequest();
+                xhr.open('DELETE',url2,true);
+                xhr.onload=()=>{
+                    if(xhr.status==200||xhr.readyState==4)
+                    {
+                        console.log('Artist deleted');
+                    }
+                    else
+                    {
+                        console.log('Error occured');
+                    }
+                }
+                xhr.send(params);
+            });
         });
         }
         
